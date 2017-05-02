012f801034ac5afc7f817ac15f56844b291d6d1a
Rong Kai Chen <ez32451@gmail.com>
2017-05-02 22:53:29 +0800

    add , update,  modify ,delete article; add lib mongoose ; register, login, logout function

--- routes/apis.js
+++ routes/apis.js
@@ -1,32 +1,76 @@
+require('../lib/db');
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Blog = mongoose.model('Blog');
+var Comment = mongoose.model('Comment');
 
-/*使用者刪除文章功能*/
-router.get('/delete/:id', function(req, res, next){
-   res.send('This is the delete function!'); 
-});
 /*使用者登入會員功能*/
-router.post('/login', function(req, res, next){
-   if((!req.body.user) || (!req.body.password)) {
-       res.redirect('/users/register');
-       return;
-   }
+router.post('/login', function (req, res, next) {
+    if ((!req.body.user) || (!req.body.password)) {
+        res.redirect('/users/register');
+        return;
+    }
     req.session.name = req.body.user;
     req.session.password = req.body.password;
     req.session.logined = true;
     res.redirect('/');
 });
+
 /*使用者新增文章功能*/
-router.post('/add', function(req, res, next){
-   res.send('This is the add function!'); 
+router.post('/add', function (req, res, next) {
+    if (!req.session.name) {
+        res.redirect('/');
+        return;
+    }
+    new Blog({
+        Username: req.session.name,
+        Article: req.body.Content,
+        CreateDate: Date.now()
+    }).save(function (err) {
+        if (err) {
+            console.log('Fail to save to DB');
+            return;
+        }
+
+        console.log('Save to DB');
+    });
+
+    res.redirect('/');
 });
+
+/*使用者刪除文章功能*/
+router.get('/delete/:id', function (req, res, next) {
+    Blog.remove({ _id: req.params.id}, function (err) {
+        if (err)
+            console.log('Fail to delete article!');
+        else
+            console.log('Done');
+    });
+    res.redirect('/users/profile');
+});
+
 /*使用者更新文章功能*/
-router.post('/update/:id', function(req, res, next){
-   res.send('This is the update function!'); 
+router.post('/update/:id', function (req, res, next) {
+    if (!req.param.id) {
+        res.redirect('/');
+        return;
+    }
+    Blog.update({
+        _id: req.param.id
+    }, {
+        Article: req.body.Content
+    }, function (err) {
+        if (err)
+            console.log('Fail to update article!');
+        else
+            console.log('Done');
+    });
+    res.redirect('/users/profile');
 });
 /*文章留言功能*/
-router.post('/comment/:id', function(req, res, next){
-   res.send('This is the comment function!'); 
+router.post('/comment/:id', function (req, res, next) {
+    res.send('This is the comment function!');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
+require('../lib/db');
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Blog = mongoose.model('Blog');
+var Comment = mongoose.model('Comment');
+
 
 /*註冊頁面*/
 router.get('/register', function(req, res, next){
@@ -15,27 +20,61 @@ router.get('/signin', function(req, res, next){
        res.redirect('/');
        return;
    } 
-    res.render('users/singin')
+    res.render('users/signin')
 });
 /*登出頁面*/
 router.get('/signout', function(req, res, next){
-   res.send('This is the signout page!'); 
+    req.session.logined = false;
+    res.redirect('/');
+    res.end();
 });
 /*忘記密碼頁面*/
 router.get('/forget', function(req, res, next){
-   res.send('This is the forget page!'); 
+    if(req.session.logined){
+        res.redirect('/');
+        return;
+    }
+    res.render('users/forget');
 });
 /*使用者管理頁面*/
 router.get('/profile', function(req, res, next){
-   res.send('This is the profile page!'); 
+   if((!req.session.name) || (!req.session.logined)) {
+       res.redirect('/');
+       return;
+   }
+    res.locals.username = req.session.name;
+    res.locals.authenticated = req.session.logined;
+    Blog.find({Username: req.session.name}, function(err, blogs, count) {
+        res.render('users/profile', {
+           title: 'Blog System',
+            blogs: blogs
+        });
+    });
 });
 /*新增文章頁面*/
 router.get('/add_article', function(req, res, next){
-   res.send('This is the add_article page!'); 
+    if((!req.session.name) || (!req.session.logined)) {
+        res.redirect('/');
+        return;
+    }
+    res.locals.username = req.session.name;
+    res.locals.authenticated = req.session.logined;
+    res.render('users/add_article');
 });
 /*修改文章頁面*/
 router.get('/modify_article/:id', function(req, res, next){
-   res.send('This is the modify_article page!'); 
+   if((!req.session.name) || (!req.session.logined)) {
+       res.redirect('/');
+       return;
+   }
+    res.locals.username = req.session.name;
+    res.locals.authenticated = req.session.logined;
+    res.locals.messageID = req.params.id;
+    Blog.find({ _id: req.params.id}, function(err, blogs, count) {
+        res.render('users/modify_article', {
+           blogs: blogs
+        });
+    });
 });
 /*訪客留言頁面*/
 router.get('/message/:id', function(req, res, next){
